a99ba66c2324e6952a857e9bc5c5c239679b9637
Aymenkenway <aymenkenway414@gmail.com>
2024-01-18 00:38:36 +0100

    comimmited

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,3 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: ['vuetify'],
 })
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/Dream-Agency/' : '/',
-}
